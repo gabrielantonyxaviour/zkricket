c2fc1f2fdb502cf7d74006e82dfa6425b2a4f924
Romario Kavin <98270892+RomarioKavin1@users.noreply.github.com>
2024-04-26 02:48:25 +0530

    minor_bug

--- Frontend/src/app/layout.tsx
+++ Frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
-import Nav from "@/components/nav";
+import Nav from "@/components/Nav";
 import Footer from "@/components/Footer";
 
 const jksans = Plus_Jakarta_Sans({ subsets: ["latin"] });
