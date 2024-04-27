1e67d84c896016e163615e8faa6c19341b2dc9e7
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2024-04-28 01:33:30 +0530

    Get specific match details

--- Frontend/src/utils/fetchFixtures.ts
+++ Frontend/src/utils/fetchFixtures.ts
@@ -3,6 +3,6 @@ import { createClient } from "@/utils/supabase/server";
 export default async function fetchFixtures() {
   const supabase = createClient();
   const { data: fixture, error } = await supabase.from("fixture").select("*");
-  console.log(fixture, error);
+  if (error) throw new Error(error.message);
   return fixture;
 }
