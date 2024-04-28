f8d0d81bd6f9aea0a5af7b59f7a10501a8da6cab
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2024-04-28 22:56:43 +0530

    removed functions

--- Frontend/src/utils/fetchFixtures.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@/utils/supabase/server";
-
-export default async function fetchFixtures() {
-  const supabase = createClient();
-  const { data: fixture, error } = await supabase.from("fixture").select("*");
-  if (error) throw new Error(error.message);
-  return fixture;
-}
