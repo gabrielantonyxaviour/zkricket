355d5b9760c293a1ba408796890f4cb5a2eff860
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2024-04-29 22:25:56 +0530

    build fixes

--- Frontend/src/components/AnkrModal.tsx
+++ Frontend/src/components/AnkrModal.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import Ankr from "../../public/Ankrverify.png";
 import Image from "next/image";
 
@@ -13,24 +12,16 @@ const AnkrModal: React.FC<Props> = ({ handleVerify }) => {
       <div className="fixed inset-0 bg-black opacity-50"></div>
       <div className="relative bg-white rounded-lg p-8 max-w-md w-full shadow-lg text-black">
         <div className="flex flex-col items-center mb-6">
-          {" "}
-          {/* Center the content and add spacing */}
           <Image
             src={Ankr}
             alt="Ankr Logo"
-            className="w-full h-full object-contain mb-4" // Adjusted styling for the image
+            className="w-full h-full object-contain mb-4"
           />
-          {/* Modal content */}
-          <h2 className="text-2xl font-bold mb-4">
-            {" "}
-            {/* Adjusted font size and weight */}
-            Terms & Conditions
-          </h2>
+          <h2 className="text-2xl font-bold mb-4">Terms &amp; Conditions</h2>
           <ul className="list-disc mb-6 text-lg text-left">
-            {" "}
-            {/* Adjusted font size */}
             <li className="mb-2">
-              This app is completely decentralized, we don't own your team data.
+              This app is completely decentralized, we don&apos;t own your team
+              data.
             </li>
             <li className="mb-2">We use Chainlink to generate scores.</li>
             <li>The user must be above 18 years to use this app.</li>
